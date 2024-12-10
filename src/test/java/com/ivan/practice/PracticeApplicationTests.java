c3c62f71991107578c9da52573894f36e076323c
SaintPeter <vanydatsckiv@gmail.com>
2024-12-10 12:00:25 +0300

    spring base init

---
+++ src/test/java/com/ivan/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
